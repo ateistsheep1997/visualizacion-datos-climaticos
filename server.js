5c50fb8ec7283b66f76d2f58db59385b61e94559
ateistsheep1997 <laguayonasu2fz@hotmail.com>
2025-09-01 17:41:26 +0200

    Updated fallback route to handle 404 errors more explicitly.

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.get('/', (req, res) => {
 });
 
 // Fallback route for unmatched routes
-app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+app.use((req, res) => {
+  res.status(404).sendFile(__dirname + '/public/404.html');
 });
 
 app.listen(PORT, () => {
